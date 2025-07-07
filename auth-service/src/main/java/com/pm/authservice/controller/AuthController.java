d9bbd0d60cfef779ab1a23d686409dceaa610b59
Nimsara Nanayakkara <85825113+nimsara21@users.noreply.github.com>
2025-07-07 23:35:51 +0530

    handling typo error in authController

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.pm.authservice.controller;
 
+import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,16 @@ import java.util.Optional;
 @RestController
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @Operation(summary = "Generate token on user login")
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginResponseDTO loginResponseDTO){
-        Optional<String> tokenOptional = authService.authenticate(loginResponseDTO);
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO){
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
 
         if (tokenOptional.isEmpty()){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
