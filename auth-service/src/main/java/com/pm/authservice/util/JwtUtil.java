cfe736625c5338f6f9676f187c8333c5d8118c5d
Nimsara Nanayakkara <85825113+nimsara21@users.noreply.github.com>
2025-07-09 01:26:07 +0530

    implementing validate token endpoints

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,13 +1,17 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+
 import java.util.Base64;
 import java.util.Date;
 
@@ -32,6 +36,21 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token){
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+
+        }
+        catch (SignatureException e){
+            throw new JwtException("Invalid JWT signature");
+
+        }catch (JwtException e) {
+            throw new JwtException("Invalid JWT");
+        }
     }
 
 }
